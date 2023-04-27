d5330151b09b9b9d4007cdb15b3cee8aa7ec6f7a
EthanBackhus <97470771+EthanBackhus@users.noreply.github.com>
2023-04-27 17:01:15 -0400

    SecondCommit

---
+++ main.py
@@ -0,0 +1,92 @@
+import openpyxl
+import requests
+
+# might need to change this depending on variable names
+workbook = openpyxl.load_workbook('Links.xlsx')
+worksheet = workbook['Sheet1']
+link = "https://www.disclaimer.topstocktips.com/$"
+
+
+def populate_worksheet():
+    stuckInLoop = True
+    while(stuckInLoop):
+        enterNewLink = input("Do you want to enter a new link? (Y/N):")
+        if(enterNewLink.ToLower() == 'y'):
+            ##TODO: create logic for true loop
+
+            loop2 = True
+            while(loop2):
+                enterTwoOrThreeLetters = input("Would you like 2 letters (AA-ZZ) or 3 letters (AAA-ZZZ)? (2/3):")
+                if(enterTwoOrThreeLetters == '2'):
+                    ##do something
+
+                    populateExcelLinksAA(worksheet)
+
+                    loop2 = False
+                elif(enterTwoOrThreeLetters == '3'):
+                    ##do something
+                    loop2 = False
+                else:
+                    print("Your answer is not recognized. Please enter 2 for AA-ZZ and 3 for AAA-ZZZ: ")
+                    enterTwoOrThreeLetters = input("Would you like 2 letters (AA-ZZ) or 3 letters (AAA-ZZZ)? (2/3):")
+
+            stuckInLoop = False
+        elif(enterNewLink.ToLower() == 'n'):
+            ##TODO: create logic for false loop
+            stuckInLoop = False
+        else:
+            print("Your answer is not recognized. Please enter Y for yes and N for no: ")
+            enterNewLink = input("Do you want to enter a new link? (Y/N):")
+
+
+# might need to change this to a variable
+def save_worksheet(workbook):
+    workbook.save('Links.xlsx')
+
+
+
+def populateExcelLinksAA(worksheet, link):
+    index = 1
+    for i in range(ord('A'), ord('Z')+1):
+        for j in range(ord('A'), ord('Z')+1):
+                sequence = (chr(i) + chr(j))
+                new_link = link.replace('$', sequence)
+                worksheetIndex = 'A' + str(index)
+                worksheet[worksheetIndex] = new_link
+                index += 1
+
+
+def populateExcelLinksAAA(worksheet, link):
+    index = 1
+    for i in range(ord('A'), ord('Z')+1):
+        for j in range(ord('A'), ord('Z')+1):
+            for k in range(ord('A'), ord('Z')+1):
+                sequence = (chr(i) + chr(j) + chr(k))
+                new_link = link.replace('$', sequence)
+                worksheetIndex = 'A' + str(index)
+                worksheet[worksheetIndex] = new_link
+                index += 1
+
+
+def send_request():
+    url = "https://www.disclaimer.topstocktips.com/cr"
+    response = requests.get(url, verify=False)
+
+    if response.status_code == 200:
+        print("success")
+    else:
+        print("also a success but kinda")
+    #print(response.content)
+
+
+
+
+
+#populateExcelLinksAA(worksheet, link)
+#populateExcelLinksAAA(worksheet, link)
+
+#populate_worksheet()
+send_request()
+ 
+ 
+workbook.save('Links.xlsx')
